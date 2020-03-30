de660d276b1e1e137baa6c6cd185db7bf2f302ec
WeScript <51118782+WeScript@users.noreply.github.com>
2020-03-30 18:45:28 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -59,8 +59,8 @@ namespace LazyW2SFinder
         static void Main(string[] args)
         {
             //ExitProcess(0);
-            string readText = File.ReadAllText("C:\\BMSetup.log");
-            Console.WriteLine(readText);
+            //string readText = File.ReadAllText("C:\\BMSetup.log");
+            //Console.WriteLine(readText);
             Console.WriteLine(AppDomain.CurrentDomain.ToString());
 
             Console.WriteLine("Hello from Lazy W2S Finder :) (clean) !");
